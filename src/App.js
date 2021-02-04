12cf323d6b5703a6393185ae219a03263b0e18ac
PrayinForRain <dldnwo99@naver.com>
2021-02-04 21:02:43 +0900

    Init
    a

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from './routes/home.js'
+
+
+function App() {
+  return (
+  <HashRouter>
+    <Route path="/" exact={true} component={Home} />
+  </HashRouter>
+  );
+}
+
+export default App;
